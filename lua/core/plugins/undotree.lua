db5f7c15d1aa1f3b923f76c7ee6b92f4d60391e6
Alan Manning <Alan_Manning@Live.co.uk>
2023-12-20 06:19:52 +0000

    initial commit

--- lua/core/plugins/undotree.lua
+++ lua/core/plugins/undotree.lua
@@ -23,25 +23,3 @@ return {
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
 	end,
 }
-
--- WHAT IN THE FUCK I DONT UNDERSTAND THIS SHIT, PERSISTENT UNDO AND CONFIG FOR THIS FUCKING PLUGIN IS DONE IN THE MAIN FILE IN NVIM-DATA\LAZY\UNDOTREE\PLUGIN\UNDOTREE.VIM
--- I ADDED THESE SPECIFIC LINES AT LINE 78 TO 93
---
--- if !exists('g:undotree_WindowLayout')
---     let g:undotree_WindowLayout = 3
--- endif
---
--- if has("persistent_undo")
---    let target_path = expand('~/.undodir/UNDOTREE_PERSISTENTANCE')
---
---     " create the directory and any parent directories
---     " if the location does not exist.
---     if !isdirectory(target_path)
---         call mkdir(target_path, "p", 0700)
---     endif
---
---     let &undodir=target_path
---     set undofile
--- endif
---
--- I DONT FUCKING KNOW OR CARE ANYMORE. PLEASE IN FUTURE FIX ME!!!!!!!
